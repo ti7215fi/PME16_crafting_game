38055e35eb38352c473d859db2a8fce14e2f938b
Ferdinand Bauer <ferdinand.bauer@fh-erfurt.de>
2016-04-14 20:29:22 +0200

    clear again

---
+++ app/src/androidTest/java/ferdi/david/tim/pme16_crafting_game/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ferdi.david.tim.pme16_crafting_game;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/ferdi/david/tim/pme16_crafting_game/GameActivity.java
@@ -0,0 +1,13 @@
+package ferdi.david.tim.pme16_crafting_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
