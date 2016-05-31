d1759bf8c3b43ac4450fc4e8b5f5002538b4fb83
Tim Fischer <tim.fischer@fh-erfurt.de>
2016-05-31 11:46:42 +0200

    remove unused import

--- app/src/main/java/ferdi/david/tim/pme16_crafting_game/GameActivity.java
+++ app/src/main/java/ferdi/david/tim/pme16_crafting_game/GameActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.NonNull;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
