fdf9ec22ec0952b217ac9b79e0cfdfb2d44b7e06
Ferdinand Bauer <ferdinand.bauer@fh-erfurt.de>
2016-07-26 18:20:28 +0200

    add highscore adapter edit highscore item layout

--- app/src/main/java/ferdi/david/tim/pme16_crafting_game/HighscoreActivity.java
+++ app/src/main/java/ferdi/david/tim/pme16_crafting_game/HighscoreActivity.java
@@ -35,5 +35,65 @@ public class HighscoreActivity extends AppCompatActivity {
         highscoreList = (ListView) findViewById(R.id.lVHighscore);
     }
 
+   /* public class HighscoreAdapter extends BaseAdapter {
+        private Context context;
+
+        public HighscoreAdapter(Context c)
+        {
+            context = c;
+        }
+
+        //---returns the number of images---
+        public int getCount() {
+            return app.getItems().length;
+        }
+
+        //---returns the ID of an item---
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public class Holder {
+            TextView tvName;
+            TextView tvScore;
+        }
+
+        //---returns an ImageView view---
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+            Holder holder;
+
+            if (convertView == null) {
+                LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+                row = inflater.inflate(R.layout.highscore_item , parent, false);
+
+                holder = new Holder();
+                holder.tvName = (TextView) row.findViewById(R.id.shop_gridviewitem_text);
+                holder.tvScore = (ImageView) row.findViewById(R.id.shop_gridviewitem_image);
+
+                row.setTag(holder);
+
+            } else {
+                holder = (Holder) row.getTag();
+            }
+
+            int amountOfItem = 0;
+            if(app.getUser() != null) {
+                amountOfItem = app.getUser().getInventoryItem(position).getAmount();
+            }
+            holder.textView.setText(amountOfItem + " x");
+            holder.imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            holder.imageView.setImageResource(app.getItems()[position]);
+
+            return row;
+        }
+
+    }*/
+
 
 }
